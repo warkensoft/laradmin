f340ca7828e19fb4e889500069e7e7028c211b8e
James Warkentin <james@truthmedia.com>
2023-08-16 15:25:50 -0700

    Some default styles.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/Resources/Views/**/*.{html,js,php}"],
+  safelist: [
+    'text-2xl',
+    'text-3xl',
+    'text-white',
+    'text-black',
+    'font-bold',
+    'font-extrabold',
+    {
+      pattern: /bg-(red|green|yellow|blue|slate)-(50|100|200|300|600|700|800|900)/,
+    },
+  ],
   theme: {
     extend: {},
   },
